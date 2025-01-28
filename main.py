d775ba6d317a20c9afa8dcff0cc5351fabe6b862
FlavienBuron <flavien.buron@gmail.com>
2025-01-28 12:45:41 +0100

    Added: gitignore file
    Added a gitignore file, with the addition that raw data from datasets
    will not be commited

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 if __name__ == "__main__":
     pass
+
+    # TODO: Transform dataset into standardized format
+    # TODO: Check for the presence of adjacency data, positional data etc, or have the user use arg
+    # TODO: Add adjacency based method from GRIN
+    # TODO: Add KNN based method from MPIN
