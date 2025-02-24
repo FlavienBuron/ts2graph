e648b06759932bccc859b53f9942577c1edcb434
FlavienBuron <flavien.buron@gmail.com>
2025-02-24 09:48:21 +0100

    Added: simple graph characeristics measures
    Added some simple graph characteristics, such as connectivity, degrees,
    clustering coefficient, number of connected elements. For a quick
    comparison if the impact of the graph completion choice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from argparse import ArgumentParser, Namespace
 
+import networkx as nx
+from numpy import mean
+
 from datasets.dataloader import get_dataset
 
 
@@ -23,14 +26,28 @@ def parse_args() -> Namespace:
     return args
 
 
+def graph_characteristics(adj):
+    G = nx.from_numpy_array(adj)
+    degrees = [d for _, d in G.degree()]
+    clustering_coeff = nx.average_clustering(G)
+    n_component = nx.number_connected_components(G)
+    largest_component = max(nx.connected_components(G), key=len)
+    connectivity = len(largest_component) / G.number_of_nodes()
+    print(
+        f"Degrees: {mean(degrees)} | Clustering coefficient: {clustering_coeff} | Number Components: {n_component} | Connectivity: {connectivity}"
+    )
+
+
 def run(args: Namespace) -> None:
     dataset = get_dataset(args.dataset)
     dataloader = dataset.get_dataloader()
     adj_matrix = dataset.get_adjacency()
-    adj_matrix_knn = dataset.get_similarity_knn(k=3)
+    adj_matrix_knn = dataset.get_similarity_knn(k=5)
     print(adj_matrix.shape)
     print(adj_matrix_knn.shape)
     print(dataset.shape())
+    graph_characteristics(adj_matrix)
+    graph_characteristics(adj_matrix_knn.numpy())
 
 
 if __name__ == "__main__":
