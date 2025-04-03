53515e502e7daf6fe0dc81db7ab595ed65445864
FlavienBuron <flavien.buron@gmail.com>
2025-04-03 17:52:17 +0200

    change: tweaked the graph gen param

--- main.py
+++ main.py
@@ -171,9 +171,9 @@ def run(args: Namespace) -> None:
     )
 
     ts2net = Ts2Net()
-    adj_matrix = dataset.get_adjacency(threshold=0.3)
+    adj_matrix = dataset.get_adjacency(threshold=0.9)
     geo_edge_index, _ = dense_to_sparse(adj_matrix)
-    adj_matrix_knn = dataset.get_similarity_knn(k=3)
+    adj_matrix_knn = dataset.get_similarity_knn(k=1)
     knn_edge_index, _ = dense_to_sparse(adj_matrix_knn)
     # data_matx = ts2net.tsnet_vg(data, "nvg", num_cores=8)
 
