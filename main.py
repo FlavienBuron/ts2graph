0e403682034d69244f7019d99caac38b7478b540
FlavienBuron <flavien.buron@gmail.com>
2025-04-10 09:10:53 +0200

    fix: removed use of corrupted data

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ def run(args: Namespace) -> None:
     dataset = get_dataset(args.dataset)
     # dataset.corrupt(missing_type="perc", missing_size=50)
     dataloader = dataset.get_dataloader(
-        use_corrupted_data=True, shuffle=False, batch_size=128
+        use_corrupted_data=False, shuffle=False, batch_size=128
     )
     graph_technique, param = args.graph_technique
     param = float(param)
