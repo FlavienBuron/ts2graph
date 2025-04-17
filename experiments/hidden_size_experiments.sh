220d07278c62fa1225d115d94dc86c0a00c6d0cd
FlavienBuron <flavien.buron@gmail.com>
2025-04-17 13:16:17 +0200

    fix: unbuffered python and run wrapper

--- experiments/hidden_size_experiments.sh
+++ experiments/hidden_size_experiments.sh
@@ -1,34 +1,35 @@
-. .venv/bin/activate
+run() {
+    . .venv/bin/activate
 
-# Get current date in YYMMDD format
-DATE=$(date +%y%m%d)
-LOGFILE="./experiments/${DATE}-hs-experiments.txt"
+    # Get current date in YYMMDD format
+    DATE=$(date +%y%m%d)
+    LOGFILE="./experiments/${DATE}-hs-experiments.txt"
 
-echo "Running experiments on $DATE" >> "$LOGFILE"
+    echo "Running experiments on $DATE" >> "$LOGFILE"
 
-# Test the impact of hidden layer size
+    # Test the impact of hidden layer size
 
-# 1. Defaults 
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g one 1 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+    # 1. Defaults 
+    python -u main.py -d air -g zero 0 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 100 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
 
-# 2. Size 8 
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g one 1 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+    # 2. Size 8 
+    python -u main.py -d air -g zero 0 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 100 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
 
-# 3. Size 16
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g one 1 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+    # 3. Size 16
+    python -u main.py -d air -g zero 0 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 100 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
 
-# 4. Size 64
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g one 1 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
-
+    # 4. Size 64
+    python -u main.py -d air -g zero 0 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 100 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+}
