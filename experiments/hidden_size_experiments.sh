dad45435c5f5b67d0ed23486d280f69926949487
FlavienBuron <flavien.buron@gmail.com>
2025-04-17 13:11:22 +0200

    change: print both to console and file. Log to results

--- experiments/hidden_size_experiments.sh
+++ experiments/hidden_size_experiments.sh
@@ -2,33 +2,33 @@
 
 # Get current date in YYMMDD format
 DATE=$(date +%y%m%d)
-LOGFILE="${DATE}-hs-experiments.txt"
+LOGFILE="./experiments/${DATE}-hs-experiments.txt"
 
 echo "Running experiments on $DATE" >> "$LOGFILE"
 
 # Test the impact of hidden layer size
 
 # 1. Defaults 
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 32 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g one 1 -i 1 -e 10 -hd 32 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 32 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 32 -o 16 >> "$LOGFILE" 2>&1
+python main.py -d air -g zero 0 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g one 1 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g knn 100 -i 1 -e 10 -hd 32 -o 16 | tee -a "$LOGFILE"
 
 # 2. Size 8 
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 8 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g one 1 -i 1 -e 10 -hd 8 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 8 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 8 -o 16 >> "$LOGFILE" 2>&1
+python main.py -d air -g zero 0 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g one 1 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g knn 100 -i 1 -e 10 -hd 8 -o 16 | tee -a "$LOGFILE"
 
 # 3. Size 16
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 16 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g one 1 -i 1 -e 10 -hd 16 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 16 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 16 -o 16 >> "$LOGFILE" 2>&1
+python main.py -d air -g zero 0 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g one 1 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g knn 100 -i 1 -e 10 -hd 16 -o 16 | tee -a "$LOGFILE"
 
 # 4. Size 64
-python main.py -d air -g zero 0 -i 1 -e 10 -hd 64 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g one 1 -i 1 -e 10 -hd 64 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 64 -o 16 >> "$LOGFILE" 2>&1
-python main.py -d air -g knn 100 -i 1 -e 10 -hd 64 -o 16 >> "$LOGFILE" 2>&1
+python main.py -d air -g zero 0 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g one 1 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g loc 0.5 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
+python main.py -d air -g knn 100 -i 1 -e 10 -hd 64 -o 16 | tee -a "$LOGFILE"
 
