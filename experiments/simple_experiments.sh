8d83206a04ba1c544f559b01d637bb2e16d67300
FlavienBuron <flavien.buron@gmail.com>
2025-04-24 20:02:50 +0200

    feat: measure all 3 depths

--- experiments/simple_experiments.sh
+++ experiments/simple_experiments.sh
@@ -11,8 +11,22 @@
     # Test the impact of hidden layer size
 
     # 1. Defaults 
-    python -u main.py -d air -g zero 0 -e 10 -hd 32 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g one 1 -e 10 -hd 32 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g loc 0.5 -e 10 -hd 32 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g knn 50 -e 10 -hd 32 -v 0 | tee -a "$LOGFILE"
+    echo "Depth 1"
+    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 50 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+
+    echo "Depth 2"
+    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 2 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 2 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -e 10 -hd 32 -ln 2 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 50 -e 10 -hd 32 -ln 2 -v 0 | tee -a "$LOGFILE"
+
+    echo "Depth 3"
+    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 3 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 3 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -e 10 -hd 32 -ln 3 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 50 -e 10 -hd 32 -ln 3 -v 0 | tee -a "$LOGFILE"
+
 
