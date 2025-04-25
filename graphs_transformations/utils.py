c09c9a23b5271dbffaf5865e44b1f6b01a1cef4e
FlavienBuron <flavien.buron@gmail.com>
2025-04-25 09:21:53 +0200

    fix: use num nodes and correct return

--- graphs_transformations/utils.py
+++ graphs_transformations/utils.py
@@ -4,7 +4,10 @@ from torch_geometric.utils import get_laplacian, to_dense_adj
 
 def compute_laplacian_smoothness(x, edge_index):
     lap_edge_index, lap_edge_weight = get_laplacian(edge_index, normalization="sym")
-    laplacian = to_dense_adj(lap_edge_index, edge_attr=lap_edge_weight)
+    print(f"{lap_edge_index.shape=}")
+    laplacian = to_dense_adj(
+        lap_edge_index, edge_attr=lap_edge_weight, max_num_nodes=x.shape[1]
+    )[0]
     print(f"{laplacian.shape=} {x.shape=}")
     smoothness = torch.trace(x.T @ laplacian @ x)
     return smoothness
