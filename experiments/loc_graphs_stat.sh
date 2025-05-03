84491cd6366b59d1b7056e70649cfd490fef87fc
FlavienBuron <flavien.buron@gmail.com>
2025-05-03 08:51:47 +0200

    feat: knn and geo-loc graph stats exp.

---
+++ experiments/loc_graphs_stat.sh
@@ -0,0 +1,15 @@
+    . .venv/bin/activate
+
+    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.0 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.1 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.2 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.3 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.4 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.5 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.6 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.7 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.8 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 0.9 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
+    python -u main.py -d air -g loc 1.0 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs 1 -dt 0
