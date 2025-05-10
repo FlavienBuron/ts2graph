b17fb2dade7ce02c4bafc7ae86eacd59860f8374
FlavienBuron <flavien.buron@gmail.com>
2025-05-10 11:00:17 +0200

    fix: knn fit the new shapes

--- graphs_transformations/knn.py
+++ graphs_transformations/knn.py
@@ -13,7 +13,7 @@ def from_knn(
     # print(f"{data.var(dim=1)=} {torch.min(data.var(dim=1))=}")
     if torch.isnan(data).any():
         # data.nan_to_num_(nan=0.0)
-        means = data.nanmean(dim=1).unsqueeze(1).expand_as(data)
+        means = data.nanmean(dim=1)
         # means.nan_to_num_(0.0)
         data = torch.where(mask, data, means)
     # data = (data - data.mean()) / (data.std() + 1e-8)
@@ -23,7 +23,7 @@ def from_knn(
         raise ValueError("Data tensor contains NaN or inf values after imputation.")
 
     # Step 3: Check for identical rows
-    if torch.allclose(data, data[0].unsqueeze(0).expand_as(data)):
+    if torch.allclose(data, data[0]):
         raise ValueError("All rows in the data tensor are identical.")
     edge_index = knn_graph(x=data, k=k, loop=loop, cosine=cosine)
     return edge_index
