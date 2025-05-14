6f7d6445c97fc191b9ee847dbc52af11c923219a
FlavienBuron <flavien.buron@gmail.com>
2025-05-14 10:40:05 +0200

    feat: more epochs experiemnts

--- experiments/epochs_experiments.sh
+++ experiments/epochs_experiments.sh
@@ -19,3 +19,15 @@
     python -u main.py -d air -g one 1 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
     python -u main.py -d air -g loc 0.5 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
     python -u main.py -d air -g knn 50 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"    
+
+    # 3. e=15
+    python -u main.py -d air -g zero 0 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 50 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"    
+
+    # 4. e=20
+    python -u main.py -d air -g zero 0 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g one 1 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g loc 0.5 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    python -u main.py -d air -g knn 50 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"    
