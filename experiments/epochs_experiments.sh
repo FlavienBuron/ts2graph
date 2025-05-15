dd93fd7e3a558e25e28b61d9aca45c6e0dea0a59
FlavienBuron <flavien.buron@gmail.com>
2025-05-15 07:35:40 +0200

    feat: epochs experiments can be provided a list of epochs

--- experiments/epochs_experiments.sh
+++ experiments/epochs_experiments.sh
@@ -1,33 +1,32 @@
-    . .venv/bin/activate
+#!/bin/bash
 
-    # Get current date in YYMMDD format
-    DATE=$(date +%y%m%d)
-    LOGFILE="./experiments/results/${DATE}-e-experiments.txt"
+source .venv/bin/activate
 
-    echo "Running experiments on $DATE" >> "$LOGFILE"
+# Accept custom list of epochs from command line, or use defaults
+EPOCHS=("$@")
+if [ ${#EPOCHS[@]} -eq 0 ]; then
+    EPOCHS=(5 10 15 20)
+fi
 
-    # Test the impact of number of epochs
+DATE=$(date +%y%m%d)
+LOGFILE="./experiments/results/${DATE}-e-experiments.txt"
 
-    # 1. Defaults e=10
-    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g loc 0.5 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g knn 50 -e 10 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+echo "Running experiments on $DATE" >> "$LOGFILE"
 
-    # 2. e=5
-    python -u main.py -d air -g zero 0 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g one 1 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g loc 0.5 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g knn 50 -e 5 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"    
+# List of grouping methods and their values
+declare -A GROUPS=(
+    ["zero"]=0
+    ["one"]=1
+    ["loc"]=0.5
+    ["knn"]=50
+)
 
-    # 3. e=15
-    python -u main.py -d air -g zero 0 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g one 1 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g loc 0.5 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g knn 50 -e 15 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"    
+# Loop through epochs and groups
+for E in "${EPOCHS[@]}"; do
+    for G in "${!GROUPS[@]}"; do
+        V=${GROUPS[$G]}
+        echo "Running: -g $G $V -e $E" | tee -a "$LOGFILE"
+        python -u main.py -d air -g "$G" "$V" -e "$E" -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
+    done
+done
 
-    # 4. e=20
-    python -u main.py -d air -g zero 0 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g one 1 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g loc 0.5 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"
-    python -u main.py -d air -g knn 50 -e 20 -hd 32 -ln 1 -v 0 | tee -a "$LOGFILE"    
