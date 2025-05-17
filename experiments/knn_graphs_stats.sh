7be0bf84892e1fe8e198ed3aaa1c05f9bbaca5a6
FlavienBuron <flavien.buron@gmail.com>
2025-05-17 12:22:57 +0200

    change: adapt graph stats scripts

--- experiments/knn_graphs_stats.sh
+++ experiments/knn_graphs_stats.sh
@@ -1,23 +1,28 @@
-    . .venv/bin/activate
+.#!/bin/bash
 
-    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 1 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 2 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 3 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 4 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 5 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 6 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 7 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 8 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 9 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 10 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 20 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 30 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 40 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 50 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 60 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 70 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 80 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 90 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g knn 100 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
+. .venv/bin/activate
+
+# Accept custom list of epochs from command line, or use defaults
+FRAC=("$@")
+if [ ${#FRAC[@]} -eq 0 ]; then
+    FRAC=({1..100})
+fi
+
+HIDDEN_DIM=32
+LAYER_NUMBER=1
+SELF_LOOP=0
+USE_MLP_OUTPUT=0
+MLP_SIZE=32
+DATASET="airq_small"
+TOTAL=36
+
+for K in {1..10} 12 14 16 18 20 22 24 26 28 30 32 34 36; do
+    echo "Running: KNN $K for $DATASET"
+    python -u main.py -d $DATASET -g knn "$K" -e 1 -hd $HIDDEN_DIM -ln $LAYER_NUMBER -gs -dt -v 0
+done
+
+# for F in "${FRAC[@]}"; do
+#     K=$(awk "BEGIN { printf \"%d\", ($TOTAL * $F) / 100 }")
+#     echo "Running: KNN $K for $DATASET"
+#     python -u main.py -d $DATASET -g knn "$K" -e 1 -hd $HIDDEN_DIM -ln $LAYER_NUMBER -gs -dt -v 0
+# done
--- experiments/loc_graphs_stat.sh
+++ experiments/loc_graphs_stat.sh
@@ -1,15 +1,23 @@
-    . .venv/bin/activate
- 
-    python -u main.py -d air -g zero 0 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g one 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt 
-    python -u main.py -d air -g loc 0.0 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.1 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.2 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.3 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.4 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.5 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.6 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.7 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.8 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 0.9 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
-    python -u main.py -d air -g loc 1.0 -i 1 -e 10 -hd 32 -ln 1 -v 0 -gs -dt
+.#!/bin/bash
+
+. .venv/bin/activate
+
+# Accept custom list of epochs from command line, or use defaults
+FRAC=("$@")
+if [ ${#FRAC[@]} -eq 0 ]; then
+    FRAC=({1..100})
+fi
+
+HIDDEN_DIM=32
+LAYER_NUMBER=1
+SELF_LOOP=0
+USE_MLP_OUTPUT=0
+MLP_SIZE=32
+DATASET="airq_small"
+TOTAL=36
+
+for F in "${FRAC[@]}"; do
+    K=$(awk "BEGIN { printf \"%d\", ($F) / 100 }")
+    echo "Running: loc $K for $DATASET"
+    python -u main.py -d $DATASET -g knn "$K" -e 1 -hd $HIDDEN_DIM -ln $LAYER_NUMBER -gs -dt -v 0
+done
