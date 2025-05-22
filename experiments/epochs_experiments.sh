8c806dcb3e2c91f1f7bcd9227f9b3c8cabc2214c
FlavienBuron <flavien.buron@gmail.com>
2025-05-22 19:25:16 +0200

    change: epochs experiment has layer number in name

--- experiments/epochs_experiments.sh
+++ experiments/epochs_experiments.sh
@@ -74,7 +74,7 @@ for E in "${EPOCHS[@]}"; do
         V=${TECHNIQUES[$G]}
         echo "Running: -g $G $V -e $E" | tee -a "$LOGFILE"
         TIMESTAMP=$(date +%y%m%d_%H%M%S)
-        FILENAME="${EXP_DIR}${TIMESTAMP}_${DATASET}_${G}_${V}_${E}.json"
+        FILENAME="${EXP_DIR}${TIMESTAMP}_${DATASET}_ln${LAYER_NUMBER}_${G}_${V}_${E}.json"
         python -u main.py -d $DATASET -sp $FILENAME -g "$G" "$V" -e "$E" -hd $HIDDEN_DIM -ln $LAYER_NUMBER $USE_TEMP -v 0 | tee -a "$LOGFILE"
     done
 done
