8592488d35d879a45cf805cdceabbf15905a15d9
FlavienBuron <flavien.buron@gmail.com>
2025-05-23 18:28:22 +0200

    change: exp. script epoch take LR

--- experiments/epochs_experiments.sh
+++ experiments/epochs_experiments.sh
@@ -32,6 +32,10 @@ while [[ $# -gt 0 ]]; do
             USE_TEMPORAL=1
             shift
             ;;
+        --lr)
+            LR="$2"
+            shift 2
+            ;;
         *)
             echo "Unknown option: $1"
             exit 1
