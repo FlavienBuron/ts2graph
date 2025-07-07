9c6e703e628b5b02cdafffbc499dd081c2b0e849
FlavienBuron <flavien.buron@gmail.com>
2025-07-07 21:08:45 +0200

    fix: add itertool dependency

--- graphs_transformations/ts2graph_rs/src/graph/temporal/recurrence.rs
+++ graphs_transformations/ts2graph_rs/src/graph/temporal/recurrence.rs
@@ -4,6 +4,6 @@ use tch::{Kind, Tensor};
 fn embed_timeseries(x: &[f32], dim: usize, tau: usize) -> Result<Vec<Vec<f32>>, String> {
     let n = x.len();
     if n < (dim - 1) * tau + 1 {
-        return Err();
+        return Err("".into());
     }
 }
