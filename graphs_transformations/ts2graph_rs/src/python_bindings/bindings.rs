f685f93beb18c2dd38ae4edac9b684c078f8b7ed
FlavienBuron <flavien.buron@gmail.com>
2025-09-05 13:38:09 +0200

    debug: print rust k-hop weights

--- graphs_transformations/ts2graph_rs/src/python_bindings/bindings.rs
+++ graphs_transformations/ts2graph_rs/src/python_bindings/bindings.rs
@@ -1,7 +1,7 @@
 use super::conversions::TensorConverter;
 use crate::graph::temporal::k_hop_graph as k_hop_rs;
 use crate::graph::temporal::recurrence_graph_rs;
-use pyo3::{exceptions::PyRuntimeError, prelude::*, types::PyString};
+use pyo3::{exceptions::PyRuntimeError, prelude::*};
 
 #[pyfunction]
 #[pyo3(signature = (time_steps, num_nodes, k, bidirectional=true, decay_name=None))]
@@ -13,14 +13,9 @@ pub fn k_hop_graph(
     bidirectional: bool,
     decay_name: Option<String>,
 ) -> PyResult<(PyObject, PyObject)> {
-    let sys = py.import("sys").unwrap();
-    let stdout = sys.getattr("stdout").unwrap();
-    let write = stdout.getattr("write").unwrap();
-    write
-        .call1((PyString::new(py, "Using Rust K-Hop graph"),))
-        .unwrap();
-    stdout.getattr("flush")?.call0()?;
-    println!("Using Rust K-hop graph.Decay name = {:?}", decay_name);
+    let builtins = py.import("builtins")?;
+    let print = builtins.getattr("print")?;
+    print.call1(("Using Rust K-Hop graph",))?;
     let decay_name_str = decay_name.as_deref();
     let (edge_index, edge_weight) =
         k_hop_rs(time_steps, num_nodes, k, bidirectional, decay_name_str)
