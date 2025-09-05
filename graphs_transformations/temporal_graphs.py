ee7b3639c0b09bccf0068e319deb2ae22a53cf61
FlavienBuron <flavien.buron@gmail.com>
2025-09-05 12:51:35 +0200

    debug: print rust k-hop weights

--- graphs_transformations/temporal_graphs.py
+++ graphs_transformations/temporal_graphs.py
@@ -24,7 +24,7 @@ def k_hop_graph_rs(
 
     edge_index = torch.from_numpy(edge_index).long()
     edge_weight = torch.from_numpy(edge_weight).float()
-    print(f"{edge_index.shape=} {edge_weight.shape=} {edge_index=}")
+
     return edge_index, edge_weight
 
 
@@ -53,7 +53,6 @@ def k_hop_graph(
     """Construct a k-hop temporal graph: t -> {t+1, ..., t+k} over a dataset of shape [T, N, F],
     where each of the N nodes is a time series of length T
     """
-    print("lol")
     time_steps, _ = x.shape
     if k == 0 or time_steps < 2:
         edge_index = torch.empty((2, 0), dtype=torch.long)  # Empty edge_index
