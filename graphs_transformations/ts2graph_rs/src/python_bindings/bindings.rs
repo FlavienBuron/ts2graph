421a539e0f19903f889c24adf1a987ff8bf0026e
FlavienBuron <flavien.buron@gmail.com>
2025-09-05 13:22:23 +0200

    debug: print rust k-hop weights

--- graphs_transformations/ts2graph_rs/src/python_bindings/bindings.rs
+++ graphs_transformations/ts2graph_rs/src/python_bindings/bindings.rs
@@ -14,8 +14,8 @@ pub fn k_hop_graph(
     bidirectional: bool,
     decay_name: Option<String>,
 ) -> PyResult<(PyObject, PyObject)> {
-    std::io::stdout().flush().unwrap();
     println!("Using Rust K-hop graph.Decay name = {:?}", decay_name);
+    std::io::stdout().flush().unwrap();
     let decay_name_str = decay_name.as_deref();
     let (edge_index, edge_weight) =
         k_hop_rs(time_steps, num_nodes, k, bidirectional, decay_name_str)
