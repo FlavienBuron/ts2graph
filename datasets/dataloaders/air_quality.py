df6661f7b95d0bdac7dad446e36e61ee0c7fa30b
FlavienBuron <flavien.buron@gmail.com>
2025-11-19 13:41:57 +0100

    test: cast val_idx to bool

--- datasets/dataloaders/air_quality.py
+++ datasets/dataloaders/air_quality.py
@@ -468,7 +468,7 @@ class AirQualityLoader(GraphLoader):
             ]
             val_idxs = np.concatenate(month_val_idxs) % len(self)
             val_idxs = val_idxs.astype(bool)
-            print(f"{len(month_val_idxs)=}")
+            print(f"{len(self)=}")
             assert isinstance(val_idxs, np.ndarray)
             assert val_idxs.dtype == bool
             assert val_idxs.shape == (len(self),)
