9991fece0a2a6bbb3cb4042f2733d1310ed5175b
FlavienBuron <flavien.buron@gmail.com>
2025-12-18 09:14:30 +0100

    fix: convert batch mask to bool (from uint)

--- downstream/imputation/imputer.py
+++ downstream/imputation/imputer.py
@@ -125,8 +125,7 @@ class Imputer:
             mask.clone().detach().float() * self.keep_prob
         ).byte()
         eval_mask = batch_data.pop("eval_mask")
-        print(f"{mask.dtype=} {eval_mask.dtype=} {batch_data["mask"].dtype=}")
-        eval_mask = (mask | eval_mask) - batch_data["mask"]
+        eval_mask = (mask | eval_mask) & ~batch_data["mask"].bool()
 
         y = batch_data.pop("y")
 
