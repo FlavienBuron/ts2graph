b971333702c19aa1b27777b868f80beb867c8780
FlavienBuron <flavien.buron@gmail.com>
2026-01-02 11:29:39 +0100

    fix: batch_data mask reconv to bool for safety

--- downstream/imputation/imputer.py
+++ downstream/imputation/imputer.py
@@ -202,6 +202,7 @@ class Imputer(pl.LightningModule):
         ).byte()
         eval_mask = batch_data.pop("eval_mask")
         eval_mask = (mask.byte() | eval_mask.byte()) - batch_data["mask"]
+        batch_data["mask"] = batch_data["mask"].bool()
 
         y = batch_data.pop("y")
 
