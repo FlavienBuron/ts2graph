b254decd478074e7ea79f78dd4fe1f00eccca025
FlavienBuron <flavien.buron@gmail.com>
2026-01-03 09:02:51 +0100

    test: compare numpy and torch scaler

--- datasets/scalers/standard_scaler.py
+++ datasets/scalers/standard_scaler.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+import numpy as np
 import torch
 
 from datasets.scalers.abstract_scaler import AbstractScaler
@@ -23,8 +24,22 @@ class StandardScaler(AbstractScaler):
 
     def fit(self, x: torch.Tensor, mask=None, keepdims: bool = True):
         if mask is not None:
-            self.bias = torch_nanmean(x, mask, axis=self.axis, keepdims=keepdims)
-            self.scale = torch_nanstd(x, mask, axis=self.axis, keepdims=keepdims)
+            x_np = x.numpy()
+            x_np = np.where(mask.numpy(), x_np, np.nan)
+            self.bias = np.nanmean(x_np, axis=self.axis, keepdims=keepdims)
+            self.scale = np.nanstd(x_np, axis=self.axis, keepdims=keepdims)
+            self.bias = torch.from_numpy(self.bias)
+            self.scale = torch.from_numpy(self.scale)
+            print(
+                f"{self.bias.min()=} {self.bias.max()=} {self.bias.mean()=} {self.bias.std()=}"
+            )
+            print(
+                f"{self.scale.min()=} {self.scale.max()=} {self.scale.mean()=} {self.scale.std()=}"
+            )
+            bias = torch_nanmean(x, mask, axis=self.axis, keepdims=keepdims)
+            scale = torch_nanstd(x, mask, axis=self.axis, keepdims=keepdims)
+            print(f"{bias.min()=} {bias.max()=} {bias.mean()=} {bias.std()=}")
+            print(f"{scale.min()=} {scale.max()=} {scale.mean()=} {scale.std()=}")
 
         else:
             self.bias = x.mean(axis=self.axis, keepdims=keepdims)
