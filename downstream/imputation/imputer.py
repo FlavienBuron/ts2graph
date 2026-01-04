da6f99dba271aa64fa8dcea5a3b527da9bcc7c5f
FlavienBuron <flavien.buron@gmail.com>
2026-01-04 20:28:35 +0100

    debug: more MAD!! GAAAAAH

--- downstream/imputation/imputer.py
+++ downstream/imputation/imputer.py
@@ -302,8 +302,8 @@ class Imputer(pl.LightningModule):
 
         masked_imp = torch.where(eval_mask, imputation, torch.tensor(float("nan")))
         masked_tar = torch.where(eval_mask, target, torch.tensor(float("nan")))
-        print("Masked imputation:", masked_imp[0, :5, :5, 0])
-        print("Masked target:", masked_tar[0, :5, :5, 0])
+        print("Masked imputation:", masked_imp[0, :15, :15, 0])
+        print("Masked target:", masked_tar[0, :15, :15, 0])
         # mad = (imputation - target).abs()
         # mad = mad[eval_mask].mean()
         # print(f"DEBUG: MAD val {mad=}")
