34af56c3b25a3c30610359f61dc0a3db0e29e82f
FlavienBuron <flavien.buron@gmail.com>
2026-01-04 20:36:55 +0100

    test: try using copy of grin scaöer

--- datasets/datamodule.py
+++ datasets/datamodule.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader, RandomSampler, Subset
 
 from datasets.dataloaders.graphloader import GraphLoader
 from datasets.scalers.abstract_scaler import AbstractScaler
+from datasets.scalers.grin_scaler import StandardScaler as GSS
 from datasets.scalers.min_max_scaler import MinMaxScaler
 from datasets.scalers.standard_scaler import StandardScaler
 
@@ -137,8 +138,9 @@ class DataModule(pl.LightningDataModule):
 
     def get_scaler(self, axis) -> AbstractScaler:
         if self.scaling_type == "std":
-            return StandardScaler(axis=axis)
+            return GSS(axis=axis)
         elif self.scaling_type == "minmax":
             return MinMaxScaler(axis=axis)
         else:
+            StandardScaler()
             raise NotImplementedError
