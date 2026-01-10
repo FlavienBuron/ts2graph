9aa42f568ed4f4ec171c74b41e78f7f29c654479
FlavienBuron <flavien.buron@gmail.com>
2026-01-10 10:28:07 +0100

    debug; check masks

--- datasets/dataloaders/graphloader.py
+++ datasets/dataloaders/graphloader.py
@@ -200,13 +200,17 @@ class GraphLoader(Dataset, ABC):
 
     def get(self, item: int, preprocess: bool = False):
         idx = self._indices[item]
-        print(f"{idx=} {idx+self.window=}")
+        # print(f"{idx=} {idx+self.window=}")
         res, transform = dict(), dict()
         if self.window > 0:
             res["x"] = self.data[idx : idx + self.window]
             for attr in self._exo_window_keys:
                 key = attr if attr not in self._exo_common_keys else (attr + "_window")
                 res[key] = getattr(self, attr)[idx : idx + self.window]
+            print(
+                "eval ⊆ mask            :",
+                (~self.mask[1527:1563] & self.eval_mask[1527:1563]).sum().item() == 0,
+            )
 
             # debug_mask_relationship(res["mask"], res["eval_mask"], "get")
 
