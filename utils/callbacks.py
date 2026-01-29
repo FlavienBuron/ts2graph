ddcadac3ed5815d560d85e83e5b0a48a971524e5
FlavienBuron <flavien.buron@gmail.com>
2026-01-29 10:23:23 +0100

    change: adapt trainer callback print

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -10,11 +10,7 @@ class ConsoleMetricsCallback(pl.Callback):
         val_metrics = {k: v.item() for k, v in metrics.items()}
         ordered = sorted(val_metrics.items())
 
-        line = (
-            f"[Epoch {trainer.current_epoch}] "
-            # f"train_mae={metrics['train_mae']:.3f} | "
-            # f"val_mae={metrics['val_mae']:.3f}"
-        )
+        line = f"[Epoch {trainer.current_epoch}] "
         line += " | ".join(f"{k}={v:.3f}" for k, v in ordered)
 
         trainer.print(line)
