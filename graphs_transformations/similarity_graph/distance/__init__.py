5c773f5901444d826f12d9b39b5eb9289f4b1486
FlavienBuron <flavien.buron@gmail.com>
2026-01-30 16:59:18 +0100

    test: remove init files

--- graphs_transformations/similarity_graph/distance/__init__.py
+++ graphs_transformations/similarity_graph/distance/__init__.py
@@ -1,8 +1,8 @@
-from .base import DistanceFunction
-from .masked_euclidean import MaskedEuclidean, NormalizedMaskedEuclidean
-
-__all__ = [
-    "DistanceFunction",
-    "MaskedEuclidean",
-    "NormalizedMaskedEuclidean",
-]
+# from .base import DistanceFunction
+# from .masked_euclidean import MaskedEuclidean, NormalizedMaskedEuclidean
+#
+# __all__ = [
+#     "DistanceFunction",
+#     "MaskedEuclidean",
+#     "NormalizedMaskedEuclidean",
+# ]
