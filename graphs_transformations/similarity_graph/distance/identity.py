cb97f4b559421d54f4fd72514f975a10274c733e
FlavienBuron <flavien.buron@gmail.com>
2026-01-30 19:28:51 +0100

    test: check loc

--- graphs_transformations/similarity_graph/distance/identity.py
+++ graphs_transformations/similarity_graph/distance/identity.py
@@ -6,5 +6,8 @@ from .base import DistanceFunction
 
 @register_distance("identity")
 class Identity(DistanceFunction):
-    def __call__(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+    def __init__(self, _):
+        pass
+
+    def __call__(self, x: torch.Tensor, _: torch.Tensor) -> torch.Tensor:
         return x.detach().clone()
