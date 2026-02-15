8a83721da5bed7e7b9dddfcebc4984352c98bbac
FlavienBuron <flavien.buron@gmail.com>
2026-02-15 11:55:19 +0100

    fix: properly import identity in affinity

--- graphs_transformations/similarity_graph/affinity/__init__.py
+++ graphs_transformations/similarity_graph/affinity/__init__.py
@@ -1,7 +1,9 @@
 from .base import AffinityFunction
 from .gaussian_kernel import GaussianKernel
+from .identity import Identity
 
 __all__ = [
     "AffinityFunction",
     "GaussianKernel",
+    "Identity",
 ]
