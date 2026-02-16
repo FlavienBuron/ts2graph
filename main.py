466220d75d8343281d233fe9d91b1bbe8aee31f3
FlavienBuron <flavien.buron@gmail.com>
2026-02-16 07:59:33 +0100

    debug: check retrieved param_val

--- main.py
+++ main.py
@@ -173,9 +173,8 @@ def run(cfg: DictConfig) -> None:
 
     param_name = next(iter(sparsifier), None)  # first key in sparsifier dict
     param_val = sparsifier["param"]
-    print(f"DEBUG: {type(param_val)=}")
-    if isinstance(param_val, dict):
-        print(f"DEBUG: {str(param_val.get('value', 'null'))=}")
+    if isinstance(param_val, DictConfig):
+        print("lsnfgksn")
         cfg.graph.label = str(param_val.get("value", "null"))
     else:
         cfg.graph.label = str(param_val)
