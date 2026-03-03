2048274d0d83528e60d409bf5aa70c9df81d5fd5
FlavienBuron <flavien.buron@gmail.com>
2026-03-03 21:05:36 +0100

    debug; check normalization

--- datasets/dataloaders/air_quality.py
+++ datasets/dataloaders/air_quality.py
@@ -31,7 +31,7 @@ class AirQualityLoader(GraphLoader):
         self.test_months = [3, 6, 9, 12]
         self.infer_eval_from = "next"
 
-        print(f"DEBUG: {horizon=} {window=}")
+        print(f"DEBUG: {horizon=} {window=} {normalization_type=}")
 
         data, missing_mask, distances = self.load(
             impute_nans=impute_nans,
