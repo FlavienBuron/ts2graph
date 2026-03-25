2e661d6bac82792761a664b8d7a67e22515e92ce
FlavienBuron <flavien.buron@gmail.com>
2026-03-25 14:05:52 +0100

    debug: dataset process init

--- datasets/dataloaders/graphloader.py
+++ datasets/dataloaders/graphloader.py
@@ -20,6 +20,7 @@ class GraphLoader(Dataset, ABC):
         horizon: int = 36,
         window: int = 36,
     ) -> None:
+        print(f"DEBUG: {type(eval_mask)=} {eval_mask is None=}")
         self.eval_mask = self._check_input(torch.tensor(eval_mask))
         self._exogenous_keys = dict()
         self._reserved_signature = {"data", "trend", "x", "y"}
