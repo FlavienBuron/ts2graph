bdce04a2dd47a5f66e72d0e63b97d5728e4321f2
FlavienBuron <flavien.buron@gmail.com>
2026-04-23 11:19:39 +0200

    test(stgi): LekyReLu instead of ReLu

--- downstream/imputation/models/STGI/stgi.py
+++ downstream/imputation/models/STGI/stgi.py
@@ -125,8 +125,8 @@ class STGI(nn.Module):
                     x_flat = gnn_layer(x_flat, self.spatial_edge_index)
 
                 if i < len(self.gnn_layers) - 1:
-                    x_flat = F.relu(x_flat)
-                    # x_flat = F.leaky_relu(x_flat, negative_slope=0.01)
+                    # x_flat = F.relu(x_flat)
+                    x_flat = F.leaky_relu(x_flat, negative_slope=0.01)
 
             # back to [B, N, S]
             x_c = rearrange(x_flat, "(b s) n 1 -> b n s", b=B, s=S)
