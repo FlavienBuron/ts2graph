0c8c6d834b680c188a4ed0d4f50f26824f3abbfb
FlavienBuron <flavien.buron@gmail.com>
2026-06-06 09:36:30 +0200

    fix: add chebychev distance to registry

--- graphs_transformations/similarity_graph/distance/__init__.py
+++ graphs_transformations/similarity_graph/distance/__init__.py
@@ -1,11 +1,13 @@
 from .base import DistanceFunction
 from .identity import Identity
 from .masked_canberra import MaskedCanberra
+from .masked_chebyshev import MaskedChebyshev
 from .masked_euclidean import MaskedEuclidean
 
 __all__ = [
     "DistanceFunction",
     "MaskedEuclidean",
     "MaskedCanberra",
+    "MaskedChebyshev",
     "Identity",
 ]
