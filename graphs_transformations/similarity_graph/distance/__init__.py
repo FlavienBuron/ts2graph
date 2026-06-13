a9bdee9cbbaf071a6510fab9c0bf3e40be4682a4
FlavienBuron <flavien.buron@gmail.com>
2026-06-13 09:35:52 +0200

    fix: add sliding distances to register

--- graphs_transformations/similarity_graph/distance/__init__.py
+++ graphs_transformations/similarity_graph/distance/__init__.py
@@ -3,11 +3,17 @@ from .identity import Identity
 from .masked_canberra import MaskedCanberra
 from .masked_chebyshev import MaskedChebyshev
 from .masked_euclidean import MaskedEuclidean
+from .sliding_canberra import SlidingCanberra
+from .sliding_euclidean import SlidingEuclidean
+from .sliding_huber import SlidingHuber
 
 __all__ = [
     "DistanceFunction",
     "MaskedEuclidean",
     "MaskedCanberra",
     "MaskedChebyshev",
+    "SlidingEuclidean",
+    "SlidingCanberra",
+    "SlidingHuber",
     "Identity",
 ]
